6e720ebc69468b053fc5f2332445dfedb81e02a9
Student <student@ms.boyslatin.org>
2023-04-20 08:32:41 -0400

    first commit

---
+++ pythonClass.py
@@ -0,0 +1,11 @@
+class Milkshake:
+     def __init__(self,ingredientsA, ingredientsB, ingredientsC):
+        self. ingredientsA = ingredientsA
+        self. ingredientsB = ingredientsB
+        self. ingredientsC = ingredientsC
+
+order= Milkshake ('Oreos', 'Ice cream', 'Milk')
+
+print(order.ingredientsA)
+print(order.ingredientsB)
+print(order.ingredientsC)
