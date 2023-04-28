f9c403478b0fc7a0bd338fe28a29594b9bb758d9
nrobinson303 <130467789+nrobinson303@users.noreply.github.com>
2023-04-28 09:37:00 -0400

    Create Pop_Quiz.py

---
+++ Pop_Quiz.py
@@ -0,0 +1,11 @@
+#Functions are integral parts of every programming language because they help make your code more modular and reusable.
+#The method operates the data in the class, while a function is used to return or pass the data
+#What is a Python dictionary? Describe a scenario when you would use a dictionary.
+#GitHub is a website that engineers use to save code and let other people see what they create 3 reasons wht GitHub is good because we can save our code public and show off our code and can find new codes to let everyone see the new code that you have found
+#Classes build data usually code and dictionaries store key-values
+cities = {Boston : "MA", Seattle : "WA", Los_Angeles : "CA", Nashville : "TN", Washinton : "DC"}
+
+
+
+#Syntax error, indent error, or spelling error
+#No parenthesis,
